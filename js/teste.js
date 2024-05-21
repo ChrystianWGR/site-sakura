f451b0574cb1b1d05fa723847fbf2268a17d1e7d
Chrystian <c.ramos@isulpar.edu.br>
2024-05-21 11:26:34 -0300

    atualizacao

--- js/teste.js
+++ js/teste.js
@@ -1,13 +1,14 @@
-const myObserver = new IntersectionObserver( (entries) => {
-    entries.forEach( (entry) =>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            entry.target.classList.remove('show')
-        }
-    })
-})
-
-const elements = document.querySelectorAll('.hidden')
-
-elements.forEach((element) => myObserver.observe(element))
+const myObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target); // Remove a observação depois que a imagem é vista
+      }
+    });
+  }, {
+    threshold: 0.5 // Define o limite de visibilidade como metade do elemento visível
+  });
+  
+  const elements = document.querySelectorAll('.hidden');
+  
+  elements.forEach((element) => myObserver.observe(element));  
