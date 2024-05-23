0e8be7dd74897267a9385001c8ca77c06e55b3e8
ChrystianWGR <c.ramos@isulpar.edu.br>
2024-05-23 12:34:37 -0300

    atualização

--- js/teste.js
+++ js/teste.js
@@ -1,14 +1,14 @@
-const myObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-        observer.unobserve(entry.target); // Remove a observação depois que a imagem é vista
-      }
+document.addEventListener('DOMContentLoaded', () => {
+    const myObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
     });
-  }, {
-    threshold: 0.5 // Define o limite de visibilidade como metade do elemento visível
-  });
-  
-  const elements = document.querySelectorAll('.hidden');
-  
-  elements.forEach((element) => myObserver.observe(element));  
+
+    const elements = document.querySelectorAll('.hidden');
+    elements.forEach((element) => myObserver.observe(element));
+});
